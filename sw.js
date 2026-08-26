eff312c45e686d5c89e3824ab99e4a6306a26491
dotsentham04549-cloud <dotsentham04549@gmail.com>
2026-08-26 14:10:26 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* ALL IN · офлайн-кэш */
-const CACHE = 'allin-v2';
+const CACHE = 'allin-v3';
 const CORE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
